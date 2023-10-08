6c353a3dbc78ed757a01e8e2cb652e39852665bb
kimsujin0812 <kimsujin0812@gmail.com>
2023-10-08 21:13:44 +0900

    modua 프로젝트 sts mapper 커밋

---
+++ modua_mapper/src/test/java/org/zerock/mapper/MemberMapperTests.java
@@ -0,0 +1,35 @@
+package org.zerock.mapper;
+
+import static org.junit.Assert.fail;
+
+import java.sql.Connection;
+
+import org.apache.ibatis.session.SqlSession;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.zerock.domain.MemberVO;
+
+import lombok.Setter;
+import lombok.extern.log4j.Log4j;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
+@Log4j
+public class MemberMapperTests {
+	
+	@Setter(onMethod_ = @Autowired)
+	private MemberMapper mapper;
+	
+	@Test
+	public void testRead() {
+		String name = "모두아";
+		String email = "modua@gamil.com";
+		
+		MemberVO member = mapper.read(name, email);
+		log.info(member);
+	}
+
+}
