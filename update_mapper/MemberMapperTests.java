b8433946c330e792df76413b3227ef769602ea62
prettypark97@gmail.com <prettypark97@gmail.com>
2023-10-09 14:37:01 +0900

    update_commit

---
+++ update_mapper/MemberMapperTests.java
@@ -0,0 +1,42 @@
+package org.zerock.mapper;
+
+
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.zerock.domain.MemberVO;
+
+import lombok.Setter;
+import lombok.extern.log4j.Log4j;
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
+@Log4j
+public class MemberMapperTests {
+	
+	@Setter(onMethod_ = @Autowired)
+	private MemberMapper mapper;
+	
+	@Test
+	public void testRead() {
+		MemberVO member = mapper.read("bbb");
+		log.info(member);		
+	}
+
+	@Test
+	public void testUpdate() {
+		MemberVO member = new MemberVO();
+		member.setMemberid("bbb");
+		member.setEmail("kim@naver.com");
+		member.setName("김연아");
+		member.setPassword("1027");
+		
+		int count = mapper.update(member);
+		log.info("UPDATE COUNT : " + count);
+	}
+
+}
---
+++ update_mapper/MemberVO.java
@@ -0,0 +1,12 @@
+package org.zerock.domain;
+
+import lombok.Data;
+
+@Data
+public class MemberVO {
+
+	public String memberid;
+	public String name;
+	public String email;
+	public String password;
+}
